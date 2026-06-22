f08cfec75c9876a8ab57b99736c10a548249d709
Desmond <24415dl@hvhs.school.nz>
2026-06-22 21:55:01 +0000

    commit

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello world!")
+function startOrder() {
+    let nameInput = document.getElementById("nameInput").value;
+    let message = "Order started for " + nameInput + "!";
+    document.getElementById("message").innerHTML = message;
+}
