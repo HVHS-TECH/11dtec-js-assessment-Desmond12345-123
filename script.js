8d3107b77c59fdaaac60ead4651d6dd2d6f4183f
Desmond <24415dl@hvhs.school.nz>
2026-07-20 06:15:49 +0000

    Jul 20, 2026, 6:15 AM

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function getFirstname() {
     const firstName = firstNameField.value;
     output.innerHTML += "<p>Hello, " + firstName + "!</p>"
 }       
+
+
+function getLastname() {
+    const lastNameField = document.getElementById("lastName");
+    const lastName = lastNameField.value;
+    output.innerHTML += "<p>Hello, " + lastName + "!</p>"
+}       
