6e24ec83d2a250361f16cfa06593d1e18606db1d
Desmond <24415dl@hvhs.school.nz>
2026-07-20 06:26:42 +0000

    Jul 20, 2026, 6:26 AM

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 const output = document.getElementById("outputMessage");
 
-
-
+// First and last name input fields1
 function getFirstname() {
     const firstNameField = document.getElementById("firstName");
     const firstName = firstNameField.value;
     output.innerHTML += "<p>Hello, " + firstName + "!</p>"
 }       
-
-
 function getLastname() {
     const lastNameField = document.getElementById("lastName");
     const lastName = lastNameField.value;
     output.innerHTML += "<p>Hello, " + lastName + "!</p>"
 }       
-
-
 // Function to open the cart
 function openCart() {
     output.innerHTML += "<p>Cart opened!</p>";
