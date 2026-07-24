8b8704044aff636f643a7f2cdbb6273f63468a38
Desmond <24415dl@hvhs.school.nz>
2026-07-24 02:34:49 +0000

    Jul 24, 2026, 2:34 AM

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const output = document.getElementById("outputMessage");
 
-let cart = [];
+let cart = [
+    {name: "Classic Glazed", amount: 0},
+    {name: "Chocolate Sprinkles", amount: 0},
+    {name: "Strawberry Frosted", amount: 0},
+    {name: "Matcha Green Tea", amount: 0},
+    {name: "Cookies and Cream", amount: 0},
+    {name: "Lotus Biscoff", amount: 0},
+    {name: "Salted Caramel", amount: 0},
+    {name: "Pistachio", amount: 0},
+    {name: "Blueberry Cheesecake", amount: 0},
+    {name: "Tiramisu", amount: 0},
+    {name: "Choco Hazelnut", amount: 0},
+    {name: "Red Velvet", amount: 0}
+];
 
 let menuItems = [
-    { name: "Classic Glazed", price: 5.99, amount: 0 },
-    { name: "Chocolate Sprinkles", price: 6.70, amount: 0 },
-    { name: "Strawberry Frosted", price: 6.50, amount: 0 },
-    { name: "Matcha Green Tea", price: 5.50, amount: 0 },
-    { name: "Cookies and Cream", price: 6.00, amount: 0 },
-    { name: "Lotus Biscoff", price: 6.25, amount: 0 },
-    { name: "Salted Caramel", price: 6.75, amount: 0 },
-    { name: "Pistachio", price: 6.25, amount: 0 },
-    { name: "Blueberry Cheesecake", price: 7.00, amount: 0 },
-    { name: "Tiramisu", price: 7.50, amount: 0 },
-    { name: "Choco Hazelnut", price: 7.25, amount: 0 },
-    { name: "Red Velvet", price: 7.75, amount: 0 }
+    { name: "Classic Glazed", price: 5.99 },
+    { name: "Chocolate Sprinkles", price: 6.70 },
+    { name: "Strawberry Frosted", price: 6.50 },
+    { name: "Matcha Green Tea", price: 5.50 },
+    { name: "Cookies and Cream", price: 6.00 },
+    { name: "Lotus Biscoff", price: 6.25 },
+    { name: "Salted Caramel", price: 6.75 },
+    { name: "Pistachio", price: 6.25 },
+    { name: "Blueberry Cheesecake", price: 7.00 },
+    { name: "Tiramisu", price: 7.50 },
+    { name: "Choco Hazelnut", price: 7.25 },
+    { name: "Red Velvet", price: 7.75 }
 ];
 
 
